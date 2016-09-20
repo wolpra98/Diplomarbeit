73ba0f4b342285b00852bd2d74bbfe6d53730713
Cartillius <johannes.ma@gmx.at>
2016-09-20 11:38:21 +0200

    Map Marker

--- GPS-Tracker/Form1.cs
+++ GPS-Tracker/Form1.cs
@@ -12,6 +12,8 @@ namespace GPS_Tracker
 {
   public partial class Form1 : Form
   {
+    GMap.NET.WindowsForms.GMapOverlay markers;
+
     public Form1()
     {
       InitializeComponent();
@@ -23,11 +25,17 @@ namespace GPS_Tracker
       GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerOnly;
       gMap.Position = new GMap.NET.PointLatLng(47.092240, 15.402685);
       gMap.ShowCenter = false;
+      markers = new GMap.NET.WindowsForms.GMapOverlay("markers");
     }
 
     private void OnBtnSetRouteClick(object sender, EventArgs e)
     {
-
+      GMap.NET.WindowsForms.GMapMarker marker =
+          new GMap.NET.WindowsForms.Markers.GMarkerGoogle(
+              new GMap.NET.PointLatLng(47.092240, 15.402685),
+              GMap.NET.WindowsForms.Markers.GMarkerGoogleType.arrow);
+      markers.Markers.Add(marker);
+      gMap.Overlays.Add(markers);
     }
   }
 }
