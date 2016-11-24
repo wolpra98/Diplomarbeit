d86649322f289d935d3edcb9b0ae5d88d5cc867a
Cartillius <johannes.ma@gmx.at>
2016-11-24 08:14:52 +0100

    App erstellt

---
+++ GPS-Tracker-App/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace GPS_Tracker_App
+{
+  [Activity(Label = "GPS_Tracker_App", MainLauncher = true, Icon = "@drawable/icon")]
+  public class MainActivity : Activity
+  {
+    protected override void OnCreate(Bundle bundle)
+    {
+      base.OnCreate(bundle);
+
+      // Set our view from the "main" layout resource
+      // SetContentView (Resource.Layout.Main);
+    }
+  }
+}
+
